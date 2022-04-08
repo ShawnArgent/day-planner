1cb3293c408d2b01091964cfc6aae4dbb309b8e7
Shawn Argent <kshawn001@gmail.com>
2022-04-08 01:07:16 -0700

    submit

--- Assets/script.js
+++ Assets/script.js
@@ -1,2 +1,54 @@
+$(document).ready(function () {
+    //display current date & time
+    $("#currentDay").text(moment().format("MMMM Do YYYY, h:mm:ss a")); // use of moment.js
+    //assign saveBtn click listener 
+    $(".saveBtn").on("click", function () {
+       
+        console.log(this);
+        var text = $(this).siblings(".description").val(); 
+        var time = $(this).parent().attr("id"); 
 
+        //set items to local storage
+        localStorage.setItem(time, text);
+    })
+    //get items from local storage
+    $("#hour9 .description").val(localStorage.getItem("hour9"));
+    $("#hour10 .description").val(localStorage.getItem("hour10"));
+    $("#hour11 .description").val(localStorage.getItem("hour11"));
+    $("#hour12 .description").val(localStorage.getItem("hour12"));
+    $("#hour13 .description").val(localStorage.getItem("hour13"));
+    $("#hour14 .description").val(localStorage.getItem("hour14"));
+    $("#hour15 .description").val(localStorage.getItem("hour15"));
+    $("#hour16 .description").val(localStorage.getItem("hour16"));
+    $("#hour17 .description").val(localStorage.getItem("hour17"));
 
+    function hourTracker() {
+        //get current number of hours.
+        var currentHour = moment().hour(); // use of moment.js
+
+        // loop over time blocks
+        $(".time-block").each(function () {
+            var blockHour = parseInt($(this).attr("id").split("hour")[1]);
+
+            //set past, present, future background colors
+            if (blockHour < currentHour) {
+                $(this).addClass("past");
+                $(this).removeClass("future");
+                $(this).removeClass("present");
+            }
+            else if (blockHour === currentHour) {
+                $(this).removeClass("past");
+                $(this).addClass("present");
+                $(this).removeClass("future");
+            }
+            else {
+                $(this).removeClass("present");
+                $(this).removeClass("past");
+                $(this).addClass("future");
+            }
+        })
+    }
+    
+    
+    hourTracker();
+})
